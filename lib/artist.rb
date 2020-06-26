4cd4769423737694c2760db91a52453760aa01eb
Jonathan Jopio <jonathanjopio@gmail.com>
2020-06-26 01:57:16 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,18 @@
+class Artist
+  attr_reader :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_song(name, genre)
+    name = self.
+  end
+  
+end
